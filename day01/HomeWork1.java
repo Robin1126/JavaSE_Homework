a30f41c69331a7173a6a2ad9d283d90a71f15468
Binbin Luo <luobinb7@gmail.com>
2023-03-02 21:38:10 +0100

    birthday

--- day01/HomeWork1.java
+++ day01/HomeWork1.java
@@ -2,7 +2,7 @@ public class HomeWork1 {
 	public static void main(String[] args) {
 		System.out.println("学号：11111111");
 		System.out.println("姓名：动力节点");
-		System.out.println("生日：2009年5月1日");
+		System.out.println("生日：2010年5月1日");
 		System.out.println("家庭住址：北京市大兴区亦庄大族企业湾10号楼A座三层");
 		System.out.println("welcome new branch!");
 		}
