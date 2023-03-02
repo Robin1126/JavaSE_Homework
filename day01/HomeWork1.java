4a26a048dd037435ea636ecfd26451dae145ad01
Binbin Luo <luobinb7@gmail.com>
2023-03-02 21:32:14 +0100

    delete Homework01

--- day01/HomeWork1.java
+++ day01/HomeWork1.java
@@ -4,6 +4,5 @@ public class HomeWork1 {
 		System.out.println("姓名：动力节点");
 		System.out.println("生日：2009年6月1日");
 		System.out.println("家庭住址：北京市大兴区亦庄大族企业湾10号楼A座三层");
-		System.out.println("猪宝宝");
 		}
 	}
